4c122ed9f5225fc1a6ed604cc1f175fe2bb5ec05
Dileep7222 <dileepkumar93635@gmail.com>
2025-05-17 18:40:03 +0530

    final commite

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -4,14 +4,14 @@ import NewsItem from './NewsItem'
 const NewsBoard = ({category}) => {
     const [articles ,setArticle] = useState([])
     useEffect(() => {
-        let url =`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+        let url =`${import.meta.env.VITE_API_URL}=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
         fetch(url)
         .then(responce=> responce.json())
         .then(data=>setArticle(data.articles))  
     },[category])
     return (
         <div>
-            <h2 className='text-center mt-2'>Latest <span className='badge bg-danger m-2'>News</span>in {category.toUpperCase()} Category</h2>
+            <h2 className='text-center mt-2'>Latest <span className='badge bg-danger m-2'>News</span>in {category.toUpperCase()}</h2>
             <h3 className='text-center mt-2'><span className='badge text-bg-info'></span></h3>
             {articles.map((news,index)=>{                
                 return <NewsItem key={index} title={news.title} description={news.description} url={news.url} src={news.urlToImage}/>
